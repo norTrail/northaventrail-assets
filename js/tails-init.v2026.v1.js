d6fa01b961a67e7d52402ff0ec0f8da0c1649fb0
norTrail <will@northaventrail.org>
2026-03-23 19:44:01 -0500

    Update tails-init.v2026.v1.js

--- js/tails-init.v2026.v1.js
+++ js/tails-init.v2026.v1.js
@@ -146,13 +146,10 @@ function initMap(container) {
       btn.title = "Reset map view";
       btn.setAttribute("aria-label", "Reset map view");
       btn.style.cssText = "display:flex;align-items:center;justify-content:center";
-      // Inline SVG avoids data-URI encoding issues
+      // Icon from shared sprite (assets.northaventrail.org/img/icons.svg)
       btn.innerHTML = `<svg aria-hidden="true" focusable="false"
-        xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-        viewBox="0 0 24 24" fill="none" stroke="#555" stroke-width="2"
-        stroke-linecap="round" stroke-linejoin="round">
-        <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-        <polyline points="9 22 9 12 15 12 15 22"/>
+        width="18" height="18" style="color:#555">
+        <use href="#icon-home"/>
       </svg>`;
 
       btn.onclick = () => {
