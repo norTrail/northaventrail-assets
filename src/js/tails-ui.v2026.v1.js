e56fdea6316aa463986c5a5ae2aeeca4faed84bf
norTrail <will@northaventrail.org>
2026-04-26 13:09:12 -0500

    Adding test date URL param to tails

--- src/js/tails-ui.v2026.v1.js
+++ src/js/tails-ui.v2026.v1.js
@@ -41,6 +41,8 @@ let lastVisibleZoneCode = null;
 let userHasScrolledTable = false;
 const TAILS_SHARE_HERD_PARAM = "herd";
 const TAILS_SHARE_ZONE_PARAM = "nomowzone";
+const TAILS_TEST_DATE_PARAM = "tailsTestDate";
+const TAILS_TEST_STATE_PARAM = "tailsTestState";
 let activeHerdCode = null;
 let sharedHerdLookupReady = false;
 let sharedNoMowLookupReady = false;
@@ -65,6 +67,8 @@ function buildTailsShareUrl_(options) {
 
   url.searchParams.delete(TAILS_SHARE_HERD_PARAM);
   url.searchParams.delete(TAILS_SHARE_ZONE_PARAM);
+  url.searchParams.delete(TAILS_TEST_DATE_PARAM);
+  url.searchParams.delete(TAILS_TEST_STATE_PARAM);
 
   if (herdCode) {
     url.searchParams.set(TAILS_SHARE_HERD_PARAM, herdCode);
@@ -1198,7 +1202,7 @@ function startCountdown(startDate) {
   }
 
   function tick() {
-    const now = new Date();
+    const now = window.TAILS_DATA?.getEffectiveNow?.() || new Date();
     const diff = start - now;
 
     if (diff <= 0) {
